7d69b31be98872dbe6f55c6fad9fbb27cb051107
mljrmb0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-19 14:37:46 +0200

    Update 113forLoop.c

--- 113forLoop.c
+++ 113forLoop.c
@@ -7,7 +7,8 @@ int main() {
 	// for (expression1; expression2; expression3)
 	// expression1 : première instruction exécuté. Ici, la première expression va déclarer la variable.
 	// expression2 : deuxième instruction exécuté. Ici, elle édicte que la variable nombre est inférieur ou doit rester inférieure à 11
-	// tant que la variable nombre est inférieure à 11, la boucle continue d'être itéré
+	// tant que la variable nombre est inférieure à 11, la boucle continue d'être itérée, c'est une condition indispensable pour que la boucle soit itéré
+	// si la condition n'est pas remplie, la boucle ne sera pas itérée
 	// expression3 : troisième instruction exécuté. Ici, cette instruction va être exécuté à chaque itération de la boucle for
 	for (nombre = 0; nombre < 11; nombre++)	{
 		printf("%d\n", nombre);
