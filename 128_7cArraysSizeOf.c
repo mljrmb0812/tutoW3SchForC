b1bff6119f31bd21122343c827ae5a43a1ad10a8
thomasMickael0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-20 16:19:24 +0200

    Create 128_7cArraysSizeOf.c

---
+++ 128_7cArraysSizeOf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int mesNumeros[] = {32, 23, 45, 54, 75896};
+	int variable;
+
+	for(variable = 0; variable < 3; variable++) {
+		printf("%d\n", mesNumeros[variable]);
+	}
+	
+	return 0;
+}
