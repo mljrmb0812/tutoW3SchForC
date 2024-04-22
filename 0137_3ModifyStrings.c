1d3f0532f313f4229aac980893fbfc6fa1950b06
thomasMickael0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-22 16:59:36 +0200

    Create 0137_3ModifyStrings.c

---
+++ 0137_3ModifyStrings.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	char formulesDePolitesse[] = "Bonjour tout le monde.";
+	formulesDePolitesse[2] = 'H'; // Modification du 3é caractère de la variable string 'n' par un 'H'
+	printf("%s", formulesDePolitesse);
+
+	return 0;
