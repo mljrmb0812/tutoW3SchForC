18f97ffcd8d39bf78810ea7f9f4cb2393462fbe8
thomasMickael0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-22 17:34:00 +0200

    Create 0140_6stringsFastidieux.c

---
+++ 0140_6stringsFastidieux.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	char politesse1[] = {'R', 'e', 'n', 'd', 'e', 'z', '-', 'v', 'o', 'u', 's', '\0'};
+	char politesse2[] = "Rendez-vous";
+
+	printf("%s\n", politesse1);
+	printf("%s\n", politesse2);
+
+	return 0;
+
+}
