97ad95405238c3ee0242060d1a461f53023e0f7c
thomasMickael0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-22 17:35:05 +0200

    Update 0140_6stringsFastidieux.c

--- 0140_6stringsFastidieux.c
+++ 0140_6stringsFastidieux.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char politesse1[] = {'R', 'e', 'n', 'd', 'e', 'z', '-', 'v', 'o', 'u', 's', '\0'};
+	char politesse1[] = {'R', 'e', 'n', 'd', 'e', 'z', '-', 'v', 'o', 'u', 's', '\0'}; // sans antislash-zéro affiche 2 fois accolé lorsqu'il est affiché avec printf
 	char politesse2[] = "Rendez-vous";
 
 	printf("%s\n", politesse1);
