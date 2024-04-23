0cf2cc3afad08cfdf4f28cef7b27b6ac7ae54509
MaxBacquet0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-23 17:52:23 +0200

    Create 0169_9pointersArrayMmryAddress.c

---
+++ 0169_9pointersArrayMmryAddress.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int boulier[8] = {7, 16, 15, 78, 58, 47, 72, 77};
+
+	// Get the value of the first element in boulier
+	// Affiche la valeur du premier élément du tableau boulier
+	printf("%d", *boulier);
+
+	return 0;
+}
