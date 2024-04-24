7eb46d66aeacb3eabca82c3308473b34f0f8b5bf
MaxBacquet0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-24 16:22:41 +0200

    Create 174_2voidFunction.c

---
+++ 174_2voidFunction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Créer une fonction
+// Create a function
+void maFonction() {
+	printf("Ceci est une fonction.");
+}
+
+int main() {
+	maFonction(); // Appelle la fonction
+	return 0;
+}
