c31b45c7ccbad2103856714534f226ae598338c6
MaxBacquet0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-25 16:58:36 +0200

    Create 183_11mFVoid.c

---
+++ 183_11mFVoid.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Créer une fonction / Create a function
+void mFVoid() {
+	printf("mFVoif = ma Fonction Vide, sans return 0");
+}
+
+int main() {
+	mFVoid(); // Appelle la fonction
+	return 0;
+}
