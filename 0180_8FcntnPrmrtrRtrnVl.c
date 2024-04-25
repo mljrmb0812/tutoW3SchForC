fcfabd7f14eeb608d0e286373d0675243c531362
MaxBacquet0812 <166826312+mljrmb0812@users.noreply.github.com>
2024-04-25 16:29:43 +0200

    Create 0180_8FcntnPrmrtrRtrnVl.c

---
+++ 0180_8FcntnPrmrtrRtrnVl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int mFAvcRtrnVl( int silex, int etincelle) {
+	return silex / etincelle;
+}
+
+
+int main() {
+	printf(" silex + etincelle = %d", mFAvcRtrnVl(7, 7));
+	return 0;
+}
